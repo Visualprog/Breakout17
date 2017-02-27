c746997a37af49289b7bba94be808eb8c42fc6f5
Unknown <cyen121@gmail.com>
2017-02-27 10:41:27 -0500

    Edit unity file

---
+++ Assets/script/peddle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peddle : MonoBehaviour {
+
+	Rigidbody2D rigidbody 
+	float speed = 5
+
+		 rigidBody = GetComponent<Rigidbody2D>();
+	void Update () {
+
+		float x = Input.GetAxisRaw ("Horizontal");
+		x = x * speed;
+		rigidBody.velocity = new Vector2 (x,0);
+	}
+}
