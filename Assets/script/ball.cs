4ac8506c8ed0cc2662bdc60c1f8dcddf69f36f7d
Unknown <cyen121@gmail.com>
2017-03-06 10:51:02 -0500

    Edit the ball sricpt
    Add Game over

--- Assets/script/ball.cs
+++ Assets/script/ball.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ball : MonoBehaviour {
-	public Vector2 startingVelocity = new Vector2 (30, -20);
+	public  Vector2 startingVelocity = new Vector2 (5, -5);
+	private Vector3 startingPosition;
+	public  GameObject gameOver;
+
+	int live = 3;
 
 	// Use this for initialization
 	void Start () {
+		startingPosition = transform.position;
 		GetComponent<Rigidbody2D> ().velocity = startingVelocity;
 	}
 	
@@ -15,10 +20,25 @@ public class ball : MonoBehaviour {
 		if (transform.position.y < -3.5f) {
 			GetOut ();
 		}
+		if (Input.GetButtonDown ("Jump")) {
+			GetComponent<Rigidbody2D> ().velocity = startingVelocity;	
+		}
 	}
+
 	void GetOut()
 	{
 		Debug.Log ("You  are out");
-	}
+		live = live - 1;
 
+		transform.position = startingPosition;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 ();
+
+		if (live == 0) {
+			DoGameOver ();
+		}
+	}
+	void DoGameOver ()
+	{
+		gameOver.SetActive (true);
+	}
 }
